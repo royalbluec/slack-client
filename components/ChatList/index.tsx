765ea6cfd007cdc31a7bfff47943355a7eb9024d
royalbluee <tkyeom96@gmail.com>
2022-02-14 03:03:15 +0900

    feat: Add custom scrollbar and date format

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,18 +1,24 @@
 import Chat from '@components/Chat';
 import { ChatZone, Section } from '@components/ChatList/styles';
 import { IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
